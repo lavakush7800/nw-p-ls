9fac4968b277d0c56b3a44ca63118ac5874cb5d6
lavakush7800 <lavakushgiri@gmail.com>
2021-07-10 10:22:56 +0530

    login and signup componts

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ]
 })
 export class UsersModule { }
